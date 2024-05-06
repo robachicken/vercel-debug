7eb6c2d47b6a3a8ab822ddeb4591b6453ce1dcf7
robachicken <97412395+robachicken@users.noreply.github.com>
2024-05-06 19:30:03 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,22 +1,22 @@
 #/bin/bash
-# run these commands from the same (problematic) network/machine
-# once completed, send the file to Vercel support
+# Run these commands from the affected/problematic network
+# Once completed, send the file to Vercel support
 
 echo "** STARTING **"
 
-#choose domain
+# Input affected domain
 read -p "Enter enter domain (eg vercel.com): " domain
 echo "Domain: ${domain:=vercel.com}"
 
 # Capture time/date
 echo "Timestamp: $(date)"
 
-# This shows the reporter's global IP address
+# Output the reporter's IP address
 echo "IP Info: "
 curl -s https://ipinfo.io/
 
 # Test reachability to Vercel A record
-echo "Testing 76.76.21.21: "
+echo "\nTesting 76.76.21.21: "
 ping -c 4 76.76.21.21
 traceroute -w 1 -m 30 76.76.21.21
 
@@ -28,20 +28,24 @@ do
   traceroute -w 1 -m 30 $i
 done
 
-# Dig your domain
+# Resolve affected domain
 echo "dig ${domain:=vercel.com}: "
 dig ${domain:=vercel.com}
 
-# Dig your domain via public DNS
+# Resolve affected domain via public DNS
 echo "dig ${domain:=vercel.com} via 8.8.8.8: "
 dig ${domain:=vercel.com} @8.8.8.8
 
-# curl content
+# Resolve affected domain directly
+echo "dig ${domain:=vercel.com} via trace: "
+dig ${domain:=vercel.com} +trace
+
+# Output content of affected domain
 echo "Output of ${domain:=vercel.com}"
 curl -sv https://${domain:=vercel.com}
 
-# mtr result
-# sudo mtr -wr -c 20 76.76.21.21
+# Output mtr result commented out due to Sudo requirement
+# for i in "76.76.21.21" "76.76.21.22" "76.76.21.9" "76.76.21.241" "76.76.21.164" "76.76.21.142" "76.76.21.61" "76.76.21.93" "76.76.21.123" "76.76.21.98";do echo "Testing $i" && sudo mtr -wr -c 20 $i;done
 
 echo "** FINISHED **"
 echo "File can be found at $(pwd)/vercel-debug.txt"
