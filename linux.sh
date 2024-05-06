9c73be215381d1fc0c912876aa73e59b5098b1c7
robachicken <97412395+robachicken@users.noreply.github.com>
2024-05-06 20:15:14 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -2,13 +2,18 @@
 # Run these commands from the affected/problematic network
 # Once completed, send the file to Vercel support
 
+# Ensure script has domain argument passed
+if [[ $# -eq 0 ]] ; then
+    echo "** No domain entered.**\nIf you're manually running this script please ensure the domain is added after the script name.\neg 'vercel-debug.sh yourdomain.com'"
+    exit 0
+fi
+
 echo "******************"
 echo "**** STARTING ****"
 echo "******************"
 
-# Input affected domain
-read 1\?"Enter Affected Domain: "
-echo "Domain: ${1:=vercel.com}"
+# Show affected domain
+echo "Domain: ${1}"
 
 # Capture time/date
 echo "Timestamp: $(date)"
@@ -32,20 +37,20 @@ do
 done
 
 # Resolve affected domain
-echo "dig ${1:=vercel.com} "
-dig ${1:=vercel.com}
+echo "dig ${1} "
+dig ${1}
 
 # Resolve affected domain via public DNS
-echo "dig ${1:=vercel.com} via 8.8.8.8: "
-dig ${1:=vercel.com} @8.8.8.8
+echo "dig ${1} via 8.8.8.8: "
+dig ${1} @8.8.8.8
 
 # Resolve affected domain directly
-echo "dig ${1:=vercel.com} via trace: "
-dig ${1:=vercel.com} +trace
+echo "dig ${1} via trace: "
+dig ${1} +trace
 
 # Output content of affected domain
-echo "Output of ${1:-vercel.com}"
-curl -sv https://${1:-vercel.com}
+echo "Output of ${1}"
+curl -sv https://${1}
 
 # Output mtr result commented out due to Sudo requirement
 # for i in "76.76.21.21" "76.76.21.22" "76.76.21.9" "76.76.21.241" "76.76.21.164" "76.76.21.142" "76.76.21.61" "76.76.21.93" "76.76.21.123" "76.76.21.98";do echo "Testing $i" && sudo mtr -wr -c 20 $i;done
